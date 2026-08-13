7b001c20f99b84d3bb74f76e75cfda22685f2aec
Yovko Yovkov <yyovkov@yyovkov.net>
2026-08-14 01:15:55 +0300

    update neovim configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- require("bufferline").setup({
+--   options = {
+--     always_show_bufferline = true,
+--   },
+-- })
--- config/nvim/lua/plugins/personal_settings.lua
+++ config/nvim/lua/plugins/personal_settings.lua
@@ -24,4 +24,10 @@ return {
       },
     },
   },
+  {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
 }
